52ac39afc2381deb1ed4951b88bf9f6f9379eb96
Yūki NISHIKAWA <yuki_nishikawa@digitalgear.info>
2013-07-03 09:23:47 +0900

    Update TM_like_backup.sh

--- TM_like_backup.sh
+++ TM_like_backup.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 # System backup script like the Time Machine.
 
 today=`date +%F`
 backup_dir=""
 backup_location="$backup_dir/system_$today"
+# The second from last update directory: ${link_destination[1]}
+link_destination=(`/bin/ls -1t $backup_dir | grep 'system_[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}'`)
 
 if [ ! -d "$backup_location" ]; then
 	mkdir $backup_location
@@ -13,7 +15,9 @@ START=`date +%s`
 
 #rsync -aAXv --link-dest="$backup_dir/`/bin/ls -1t $backup_dir | grep system | head -2 | tail -1`" /* $backup_location \
 #        --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/lost+found,/home/*/.gvfs}
-rsync -n -aAXv --link-dest="$backup_dir/`/bin/ls -1t $backup_dir | grep system | head -2 | tail -1`" /* $backup_location \
+#rsync -n -aAXv --link-dest="$backup_dir/`/bin/ls -1t $backup_dir | grep system | head -2 | tail -1`" /* $backup_location \
+#        --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/lost+found,/home/*/.gvfs}
+rsync -n -aAXv --link-dest="${link_destination[1]}" /* $backup_location \
         --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/lost+found,/home/*/.gvfs}
 
 FINISH=`date +%s`
